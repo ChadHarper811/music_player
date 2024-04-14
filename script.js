de060f7a0cb2f105e6bbe45d1fdaa3e48bcddcea
Chad Harper <chadharper811@gmail.com>
2024-04-13 21:16:11 -0500

    added event listener for play button and onclick to the render songs buttons

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ const playSong = (id) => {
 const renderSongs = (array) => {
     const songsHTML = array.map((song)=> {
         return `<li id="song-${song.id}" class="playlist-song">
-            <button class="playlist-song-info">
+            <button class="playlist-song-info" onclick="playSong(${song.id})">
                 <span class="playlist-song-title">${song.title}</span>
                 <span class="playlist-song-artist">${song.artist}</span>
                 <span class="playlist-song-duration">${song.duration}</span>
@@ -118,7 +118,13 @@ const renderSongs = (array) => {
     playlistSongs.innerHTML = songsHTML;
 };
 
-
+playButton.addEventListener("click",()=>{
+    if(!userData?.currentSong) {
+        playSong(userData?.songs[0].id)
+    } else {
+        playSong(userData?.currentSong.id);
+    }
+});
 
 const sortSongs = () => {
     userData?.songs.sort((a,b) =>{
