44ff984080a2427826788c48116b791edfc2265e
Chad Harper <chadharper811@gmail.com>
2024-04-22 18:38:29 -0500

    added function for displaying current song info

--- script.js
+++ script.js
@@ -98,6 +98,7 @@ const playSong = (id) => {
     userData.currentSong = song;
     playButton.classList.add("playing");
     highlightCurrentSong();
+    setPlayerDisplay();
     audio.play();
 }
 
@@ -126,6 +127,15 @@ const playPreviousSong = () => {
       playSong(previousSong.id);
     }
 };
+
+const setPlayerDisplay = () => {
+    const playingSong = document.getElementById("player-song-title");
+    const songArtist = document.getElementById("player-song-artist");
+    const currentTitle = userData?.currentSong?.title;
+    const currentArtist = userData?.currentSong?.artist;
+    playingSong.textContent = currentTitle ? currentTitle : "";
+    songArtist.textContent = currentArtist ? currentArtist : "";
+}
 
 const highlightCurrentSong = () => {
     const playlistSongElements = document.querySelectorAll(".playlist-song");
