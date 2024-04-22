e324934fb60edce5cd09ae56b35ebb0b513f8c6e
Chad Harper <chadharper811@gmail.com>
2024-04-22 18:21:11 -0500

    added function for highlighting current song

--- script.js
+++ script.js
@@ -97,6 +97,7 @@ const playSong = (id) => {
     }
     userData.currentSong = song;
     playButton.classList.add("playing");
+    highlightCurrentSong();
     audio.play();
 }
 
@@ -124,7 +125,18 @@ const playPreviousSong = () => {
       const  previousSong = userData?.songs[currentSongIndex - 1];
       playSong(previousSong.id);
     }
-}
+};
+
+const highlightCurrentSong = () => {
+    const playlistSongElements = document.querySelectorAll(".playlist-song");
+    const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
+    playlistSongElements.forEach((songEl) => {
+      songEl.removeAttribute("aria-current");
+    });
+    if (songToHighlight) {
+      songToHighlight.setAttribute("aria-current", "true");
+    }
+};
 
 const renderSongs = (array) => {
     const songsHTML = array.map((song)=> {
